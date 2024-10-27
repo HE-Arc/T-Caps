d0b09f5a08f359a460bbc17e8ae2575a0d3dfc0b
Villarejo Maxime <maxime.villarejo@he-arc.ch>
2024-10-27 20:13:17 +0100

    design navbar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,20 +15,14 @@
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body class="font-sans antialiased">
-        <div class="min-h-screen  background-app">
-            @include('layouts.navigation')
+        <div class="min-h-screen flex">
+            <!-- Barre de navigation latérale avec bordure à droite -->
+            <aside class="fixed h-full w-20 bg-gray-800 text-white flex flex-col">
+                @include('layouts.navigation')
+            </aside>
 
-            <!-- Page Heading -->
-            @isset($header)
-                <header class="bg-white dark:bg-gray-800 shadow">
-                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                        {{ $header }}
-                    </div>
-                </header>
-            @endisset
-
-            <!-- Page Content -->
-            <main>
+            <!-- Contenu principal centré -->
+            <main class="flex-1 flex items-center justify-center bg-white background-app">
                 {{ $slot }}
             </main>
         </div>
