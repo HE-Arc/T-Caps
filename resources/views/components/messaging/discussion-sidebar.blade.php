a7504beef250d96e1f682e40d3b1d9ab658ee348
Villarejo Maxime <maxime.villarejo@he-arc.ch>
2024-11-14 13:37:06 +0100

    fix side-bar design

--- resources/views/components/messaging/discussion-sidebar.blade.php
+++ resources/views/components/messaging/discussion-sidebar.blade.php
@@ -1,101 +1,163 @@
-<div class="w-1/5 secondary-background-app text-white border-r border-black p-4 h-full overflow-y-auto scrollbar-hide">
+<div class="w-1/5 secondary-background-app text-white border-r border-black overflow-y-auto h-full scrollbar-hide">
     <ul>
-        <li class="p-2 border-b border-gray-600 hover:bg-gray-700 cursor-pointer">
-            Discussion 1
+        <li class="flex items-center p-2 border-b border-black overflow-hidden mr-2">
+            <!-- Utilisation de w-full pour s'assurer que le conteneur prend toute la largeur -->
+            <div class="flex justify-between text-white w-full">
+                <div>Discussions</div>
+                <div>+</div>
+            </div>
         </li>
-        <li class="p-2 border-b border-gray-600 hover:bg-gray-700 cursor-pointer">
-            Discussion 2
+        <li class="flex items-center p-2 border-b border-black overflow-hidden mr-2">
+            <img src="source/assets/images/profile.png" alt="Avatar" class="w-10 h-10 rounded-full mr-3">
+            <div class="flex-1">
+                <div class="font-bold">Eddy</div>
+                <div class="text-sm text-gray-400 whitespace-nowrap">
+                    Un autre message très long long message dernier message envoyée
+                </div>
+            </div>
         </li>
-        <li class="p-2 border-b border-gray-600 hover:bg-gray-700 cursor-pointer">
-            Discussion 2
+
+        <li class="flex items-center p-2 border-b border-black overflow-hidden mr-2">
+            <img src="source/assets/images/profile.png" alt="Avatar" class="w-10 h-10 rounded-full mr-3">
+            <div class="flex-1">
+                <div class="font-bold">Eddy</div>
+                <div class="text-sm text-gray-400 whitespace-nowrap">
+                    Un autre message très long long message dernier message envoyée
+                </div>
+            </div>
         </li>
-        <li class="p-2 border-b border-gray-600 hover:bg-gray-700 cursor-pointer">
-            Discussion 2
+        <li class="flex items-center p-2 border-b border-black overflow-hidden mr-2">
+            <img src="source/assets/images/profile.png" alt="Avatar" class="w-10 h-10 rounded-full mr-3">
+            <div class="flex-1">
+                <div class="font-bold">Eddy</div>
+                <div class="text-sm text-gray-400 whitespace-nowrap">
+                    Un autre message très long long message dernier message envoyée
+                </div>
+            </div>
         </li>
-        <li class="p-2 border-b border-gray-600 hover:bg-gray-700 cursor-pointer">
-            Discussion 2
+
+        <li class="flex items-center p-2 border-b border-black overflow-hidden mr-2">
+            <img src="source/assets/images/profile.png" alt="Avatar" class="w-10 h-10 rounded-full mr-3">
+            <div class="flex-1">
+                <div class="font-bold">Eddy</div>
+                <div class="text-sm text-gray-400 whitespace-nowrap">
+                    Un autre message très long long message dernier message envoyée
+                </div>
+            </div>
         </li>
-        <li class="p-2 border-b border-gray-600 hover:bg-gray-700 cursor-pointer">
-            Discussion 2
+        <li class="flex items-center p-2 border-b border-black overflow-hidden mr-2">
+            <img src="source/assets/images/profile.png" alt="Avatar" class="w-10 h-10 rounded-full mr-3">
+            <div class="flex-1">
+                <div class="font-bold">Eddy</div>
+                <div class="text-sm text-gray-400 whitespace-nowrap">
+                    Un autre message très long long message dernier message envoyée
+                </div>
+            </div>
         </li>
-        <li class="p-2 border-b border-gray-600 hover:bg-gray-700 cursor-pointer">
-            Discussion 2
+
+        <li class="flex items-center p-2 border-b border-black overflow-hidden mr-2">
+            <img src="source/assets/images/profile.png" alt="Avatar" class="w-10 h-10 rounded-full mr-3">
+            <div class="flex-1">
+                <div class="font-bold">Eddy</div>
+                <div class="text-sm text-gray-400 whitespace-nowrap">
+                    Un autre message très long long message dernier message envoyée
+                </div>
+            </div>
         </li>
-        <li class="p-2 border-b border-gray-600 hover:bg-gray-700 cursor-pointer">
-            Discussion 2
+        <li class="flex items-center p-2 border-b border-black overflow-hidden mr-2">
+            <img src="source/assets/images/profile.png" alt="Avatar" class="w-10 h-10 rounded-full mr-3">
+            <div class="flex-1">
+                <div class="font-bold">Eddy</div>
+                <div class="text-sm text-gray-400 whitespace-nowrap">
+                    Un autre message très long long message dernier message envoyée
+                </div>
+            </div>
         </li>
-        <li class="p-2 border-b border-gray-600 hover:bg-gray-700 cursor-pointer">
-            Discussion 1
+
+        <li class="flex items-center p-2 border-b border-black overflow-hidden mr-2">
+            <img src="source/assets/images/profile.png" alt="Avatar" class="w-10 h-10 rounded-full mr-3">
+            <div class="flex-1">
+                <div class="font-bold">Eddy</div>
+                <div class="text-sm text-gray-400 whitespace-nowrap">
+                    Un autre message très long long message dernier message envoyée
+                </div>
+            </div>
         </li>
-        <li class="p-2 border-b border-gray-600 hover:bg-gray-700 cursor-pointer">
-            Discussion 2
+        <li class="flex items-center p-2 border-b border-black overflow-hidden mr-2">
+            <img src="source/assets/images/profile.png" alt="Avatar" class="w-10 h-10 rounded-full mr-3">
+            <div class="flex-1">
+                <div class="font-bold">Eddy</div>
+                <div class="text-sm text-gray-400 whitespace-nowrap">
+                    Un autre message très long long message dernier message envoyée
+                </div>
+            </div>
         </li>
-        <li class="p-2 border-b border-gray-600 hover:bg-gray-700 cursor-pointer">
-            Discussion 2
+
+        <li class="flex items-center p-2 border-b border-black overflow-hidden mr-2">
+            <img src="source/assets/images/profile.png" alt="Avatar" class="w-10 h-10 rounded-full mr-3">
+            <div class="flex-1">
+                <div class="font-bold">Eddy</div>
+                <div class="text-sm text-gray-400 whitespace-nowrap">
+                    Un autre message très long long message dernier message envoyée
+                </div>
+            </div>
         </li>
-        <li class="p-2 border-b border-gray-600 hover:bg-gray-700 cursor-pointer">
-            Discussion 2
+        <li class="flex items-center p-2 border-b border-black overflow-hidden mr-2">
+            <img src="source/assets/images/profile.png" alt="Avatar" class="w-10 h-10 rounded-full mr-3">
+            <div class="flex-1">
+                <div class="font-bold">Eddy</div>
+                <div class="text-sm text-gray-400 whitespace-nowrap">
+                    Un autre message très long long message dernier message envoyée
+                </div>
+            </div>
         </li>
-        <li class="p-2 border-b border-gray-600 hover:bg-gray-700 cursor-pointer">
-            Discussion 2
+
+        <li class="flex items-center p-2 border-b border-black overflow-hidden mr-2">
+            <img src="source/assets/images/profile.png" alt="Avatar" class="w-10 h-10 rounded-full mr-3">
+            <div class="flex-1">
+                <div class="font-bold">Eddy</div>
+                <div class="text-sm text-gray-400 whitespace-nowrap">
+                    Un autre message très long long message dernier message envoyée
+                </div>
+            </div>
         </li>
-        <li class="p-2 border-b border-gray-600 hover:bg-gray-700 cursor-pointer">
-            Discussion 2
+        <li class="flex items-center p-2 border-b border-black overflow-hidden mr-2">
+            <img src="source/assets/images/profile.png" alt="Avatar" class="w-10 h-10 rounded-full mr-3">
+            <div class="flex-1">
+                <div class="font-bold">Eddy</div>
+                <div class="text-sm text-gray-400 whitespace-nowrap">
+                    Un autre message très long long message dernier message envoyée
+                </div>
+            </div>
         </li>
-        <li class="p-2 border-b border-gray-600 hover:bg-gray-700 cursor-pointer">
-            Discussion 2
+
+        <li class="flex items-center p-2 border-b border-black overflow-hidden mr-2">
+            <img src="source/assets/images/profile.png" alt="Avatar" class="w-10 h-10 rounded-full mr-3">
+            <div class="flex-1">
+                <div class="font-bold">Eddy</div>
+                <div class="text-sm text-gray-400 whitespace-nowrap">
+                    Un autre message très long long message dernier message envoyée
+                </div>
+            </div>
         </li>
-        <li class="p-2 border-b border-gray-600 hover:bg-gray-700 cursor-pointer">
-            Discussion 2
+        <li class="flex items-center p-2 border-b border-black overflow-hidden mr-2">
+            <img src="source/assets/images/profile.png" alt="Avatar" class="w-10 h-10 rounded-full mr-3">
+            <div class="flex-1">
+                <div class="font-bold">Eddy</div>
+                <div class="text-sm text-gray-400 whitespace-nowrap">
+                    Un autre message très long long message dernier message envoyée
+                </div>
+            </div>
         </li>
-        <li class="p-2 border-b border-gray-600 hover:bg-gray-700 cursor-pointer">
-            Discussion 1
+
+        <li class="flex items-center p-2 border-b border-black overflow-hidden mr-2">
+            <img src="source/assets/images/profile.png" alt="Avatar" class="w-10 h-10 rounded-full mr-3">
+            <div class="flex-1">
+                <div class="font-bold">Eddy</div>
+                <div class="text-sm text-gray-400 whitespace-nowrap">
+                    Un autre message très long long message dernier message envoyée
+                </div>
+            </div>
         </li>
-        <li class="p-2 border-b border-gray-600 hover:bg-gray-700 cursor-pointer">
-            Discussion 2
-        </li>
-        <li class="p-2 border-b border-gray-600 hover:bg-gray-700 cursor-pointer">
-            Discussion 2
-        </li>
-        <li class="p-2 border-b border-gray-600 hover:bg-gray-700 cursor-pointer">
-            Discussion 2
-        </li>
-        <li class="p-2 border-b border-gray-600 hover:bg-gray-700 cursor-pointer">
-            Discussion 2
-        </li>
-        <li class="p-2 border-b border-gray-600 hover:bg-gray-700 cursor-pointer">
-            Discussion 2
-        </li>
-        <li class="p-2 border-b border-gray-600 hover:bg-gray-700 cursor-pointer">
-            Discussion 2
-        </li>
-        <li class="p-2 border-b border-gray-600 hover:bg-gray-700 cursor-pointer">
-            Discussion 2
-        </li>
-        <li class="p-2 border-b border-gray-600 hover:bg-gray-700 cursor-pointer">
-            Discussion 1
-        </li>
-        <li class="p-2 border-b border-gray-600 hover:bg-gray-700 cursor-pointer">
-            Discussion 2
-        </li>
-        <li class="p-2 border-b border-gray-600 hover:bg-gray-700 cursor-pointer">
-            Discussion 2
-        </li>
-        <li class="p-2 border-b border-gray-600 hover:bg-gray-700 cursor-pointer">
-            Discussion 2
-        </li>
-        <li class="p-2 border-b border-gray-600 hover:bg-gray-700 cursor-pointer">
-            Discussion 2
-        </li>
-        <li class="p-2 border-b border-gray-600 hover:bg-gray-700 cursor-pointer">
-            Discussion 2
-        </li>
-        <li class="p-2 border-b border-gray-600 hover:bg-gray-700 cursor-pointer">
-            Discussion 2
-        </li>
-        <li class="p-2 border-b border-gray-600 hover:bg-gray-700 cursor-pointer">
-            Discussion 2
-        </li>
-        <!-- Ajouter d'autres discussions ici -->
     </ul>
 </div>
