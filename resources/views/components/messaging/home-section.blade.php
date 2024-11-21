834154de9a011c7152c52642f5eb7d8776ebc777
Villarejo Maxime <maxime.villarejo@he-arc.ch>
2024-11-21 18:17:58 +0000

    Add welcoming component at dashboard page opening

---
+++ resources/views/components/messaging/home-section.blade.php
@@ -0,0 +1,15 @@
+<div class="flex flex-col items-center justify-center h-full text-white bg-gray-800 bg-cover bg-center messagesBackground">
+    <!-- Logo -->
+    <img src="{{ asset('source/assets/images/logo.png') }}" alt="Logo" class="w-[30%] mb-6" />
+
+    <!-- Texte descriptif de l'application -->
+    <p class="text-center text-xl font-semibold w-[75%]">
+        Bienvenue sur T-CAPS !
+    </p>
+    <p class="text-center text-xl font-semibold w-[75%]">
+        Connectez-vous avec vos amis et partagez des capsules temporelles, des messages spéciaux qu'ils pourront ouvrir à la date que vous aurez choisie.
+    </p>
+    <p class="text-center text-m font-semibold w-[75%] pt-10">
+        He-Arc Neuchâtel 2024 - Jaton David - Naddeo Eddy - Maxime Villarejo.
+    </p>
+</div>
